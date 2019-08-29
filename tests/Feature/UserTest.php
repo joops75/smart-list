e6fe4ce02ae197339b791522f5f7fdcdb42165a6
Simon Cooper <kidney9@hotmail.com>
2019-08-29 17:30:56 +0100

    added UserTest suite

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**  @test */
+
+    public function a_user_can_have_associated_projects()
+    {
+        $user = $this->login();
+
+        $this->post('/project', [
+            'title' => 'A New Project',
+            'description' => 'An awesome project',
+            'user_id' => $user->id
+        ])->assertRedirect('/project');
+
+        $this->assertEquals($user->projects()->first()->title, 'A New Project');
+    }
+}
