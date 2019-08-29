e98d9a2ed636cf995a0712aa2f743d4ea4e2a0d8
Simon Cooper <kidney9@hotmail.com>
2019-08-29 17:23:28 +0100

    associated User and Project models

--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     public $guarded = [];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
--- database/migrations/2019_08_28_140846_create_projects_table.php
+++ database/migrations/2019_08_28_140846_create_projects_table.php
@@ -15,6 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->text('description');
             $table->timestamps();
