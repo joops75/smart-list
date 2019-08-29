189337ab296c74b964a16961c80a8b2bc9f89278
Simon Cooper <kidney9@hotmail.com>
2019-08-29 17:30:32 +0100

    changed location of login code for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function login() {
+        $user = factory(\App\User::class)->create();
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
